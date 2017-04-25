7e43b9f62d232582e26d9f439b6930f952d68010
Steve Kehlet <skehlet@nextgen.com>
2017-04-24 19:26:50 -0700

    4 space indent

--- init.lua
+++ init.lua
@@ -13,49 +13,49 @@ local function reloadConfig(files)
     end
 end
 
--- note myWatcher and other variables are deliberately global so it's never garbage collected
+-- note myWatcher and other variables are deliberately global so they're never garbage collected
 myWatcher = hs.pathwatcher.new(os.getenv('HOME')..'/.hammerspoon/', reloadConfig):start()
 
 local function move(cb)
-  local win = hs.window.focusedWindow()
-  if win then
-      local frame = win:frame()
-      local screenFrame = win:screen():frame()
-      frame.x, frame.y, frame.w, frame.h = cb(frame, screenFrame)
-      local log = hs.logger.new('move', 'debug')
-      log.d(win:title()..' to '..frame.x..','..frame.y..','..frame.w..','..frame.h)
-      win:setFrame(frame)
+    local win = hs.window.focusedWindow()
+    if win then
+        local frame = win:frame()
+        local screenFrame = win:screen():frame()
+        frame.x, frame.y, frame.w, frame.h = cb(frame, screenFrame)
+        local log = hs.logger.new('move', 'debug')
+        log.d(win:title()..' to '..frame.x..','..frame.y..','..frame.w..','..frame.h)
+        win:setFrame(frame)
   end
 end
 
 hyper = hs.hotkey.modal.new()
 
 hyper:bind({}, 'f', function ()
-  move(function (f, sf) return sf.x, sf.y, sf.w, sf.h end)
+    move(function (f, sf) return sf.x, sf.y, sf.w, sf.h end)
 end)
 
 hyper:bind({}, 'c', function ()
-  move(function (f, sf)
-    local x = sf.x + ((sf.w - f.w) / 2)
-    local y = sf.y + ((sf.h - f.h) / 2)
-    return x, y, f.w, f.h
-  end)
+    move(function (f, sf)
+        local x = sf.x + ((sf.w - f.w) / 2)
+        local y = sf.y + ((sf.h - f.h) / 2)
+        return x, y, f.w, f.h
+    end)
 end)
 
 hyper:bind({}, 'left', function ()
-  move(function (f, sf) return sf.x, sf.y, sf.w/2, sf.h end)
+    move(function (f, sf) return sf.x, sf.y, sf.w/2, sf.h end)
 end)
 
 hyper:bind({}, 'right', function ()
-  move(function (f, sf) return (sf.x2 - sf.w/2), sf.y, sf.w/2, sf.h end)
+    move(function (f, sf) return (sf.x2 - sf.w/2), sf.y, sf.w/2, sf.h end)
 end)
 
 hyper:bind({}, 'up', function ()
-  move(function (f, sf) return f.x, sf.y, f.w, sf.h/2 end)
+    move(function (f, sf) return f.x, sf.y, f.w, sf.h/2 end)
 end)
 
 hyper:bind({}, 'down', function ()
-  move(function (f, sf) return f.x, (sf.y2 - sf.h/2), f.w, sf.h/2 end)
+    move(function (f, sf) return f.x, (sf.y2 - sf.h/2), f.w, sf.h/2 end)
 end)
 
 hyper:bind({}, 'r', hs.reload)
@@ -64,7 +64,7 @@ hyper:bind({}, 't', function ()
     os.execute('/usr/bin/open -a Terminal ~')
 end)
 hyper:bind({}, 'l', function ()
-  os.execute('/usr/local/bin/lockscreen')
+    os.execute('/usr/local/bin/lockscreen')
 end)
 
 -- Use Karabiner-Elements to map caps_lock to f18.

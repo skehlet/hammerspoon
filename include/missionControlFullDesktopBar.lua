88eaacb1a7979122fb1e596bacecaa1df442dbc3
Steve Kehlet <steve.kehlet@avaneerhealth.com>
2024-07-02 11:52:21 -0700

    cleanup; switch back to using -i with new build

--- include/missionControlFullDesktopBar.lua
+++ include/missionControlFullDesktopBar.lua
@@ -6,40 +6,57 @@ local eventTaps = require("lib.eventTaps")
 
 local MISSION_CONTROL_KEYCODE = 0xa0
 local MCFDB_PATH = '/Applications/missionControlFullDesktopBar.app/Contents/MacOS/missionControlFullDesktopBar'
+-- local MCFDB_PATH = '/Users/skehlet/Library/Developer/Xcode/DerivedData/missionControlFullDesktopBar-cecfmmntuhizngavuwnoabrsqvib/Build/Products/Debug/missionControlFullDesktopBar.app/Contents/MacOS/missionControlFullDesktopBar'
 
 local mcfdbSize = hs.fs.attributes(MCFDB_PATH, 'size')
 if mcfdbSize then
     logger.i('missionControlFullDesktopBar found, intercepting Mission Control key events')
-    eventTaps:createEventTap({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp}, function(event)
-        if event:getKeyCode() == MISSION_CONTROL_KEYCODE then
-            local isRepeat = event:getProperty(hs.eventtap.event.properties.keyboardEventAutorepeat)
-            if isRepeat == 1 then
-                return true -- ignore and discard
-            end
-            -- don't intercept cmd+f3 or ctrl+f3
-            local flags = event:getFlags()
-            if (flags.cmd or flags.ctrl) then
-                return false -- propogate
-            end
-            if event:getType() == hs.eventtap.event.types.keyDown then
-                -- logger.i('intercepted Mission Control DOWN')
-                if hammer.isDown then
-                    logger.i(
-                        'intercepted Hammer+Mission Control DOWN, finding and killing missionControlFullDesktopBar...')
-                    local mcfdb = hs.application.find("missionControlFullDesktopBar")
-                    if mcfdb then
-                        mcfdb:kill()
-                    end
-                end
-                -- os.execute(MCFDB_PATH..' -d -i')
-                os.execute(MCFDB_PATH .. ' -d')
-                return true -- discard
-            else
-                -- logger.i('intercepted Mission Control UP')
-                os.execute(MCFDB_PATH .. ' -d -r')
-                return true -- discard
-            end
+    eventTaps:createEventTap({
+        hs.eventtap.event.types.keyDown,
+        hs.eventtap.event.types.keyUp
+    }, function(event)
+        if event:getKeyCode() ~= MISSION_CONTROL_KEYCODE then
+            return false -- propogate
         end
-        return false -- propogate
+
+        -- don't intercept if cmd or ctrl was held down
+        local flags = event:getFlags()
+        if flags.cmd or flags.ctrl then
+            return false -- propogate
+        end
+
+        local isRepeat = event:getProperty(hs.eventtap.event.properties.keyboardEventAutorepeat)
+        if isRepeat == 1 then
+            return true -- discard
+        end
+
+        if event:getType() == hs.eventtap.event.types.keyDown then
+            onMissionControlKeyDown()
+        else
+            onMissionControlKeyUp()
+        end
+
+        return true -- discard
     end)
+end
+
+function onMissionControlKeyDown()
+    print("Mission Control key DOWN")
+    if hammer.isDown then
+        logger.i('intercepted Hammer+Mission Control DOWN, finding and killing missionControlFullDesktopBar...')
+        local mcfdb = hs.application.find("missionControlFullDesktopBar")
+        if mcfdb then
+            mcfdb:kill()
+        end
+    end
+    os.execute(MCFDB_PATH .. ' -d -i')
+end
+
+function onMissionControlKeyUp()
+    print("Mission Control key UP")
+    if hammer.isDown then
+        -- ignore
+        return
+    end
+    os.execute(MCFDB_PATH .. ' -r')
 end

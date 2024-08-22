6ef72889d2c165c98814e2b187e6f818fad4d476
Steve Kehlet <steve.kehlet@avaneerhealth.com>
2024-08-22 10:26:04 -0700

    refactor to use debouncer for all mouse 4/5 click handlers

--- include/mouseBackForward.lua
+++ include/mouseBackForward.lua
@@ -9,82 +9,89 @@ local util = require("lib.util")
 
 local OTHER_MOUSE_DOWN_DEBOUNCE_TIME = 200 -- ms
 local otherMouseDownAt = 0
+local otherMouseDownCustomHandlers = {}
 
 -- Make Hammer+mouse4/mouse5 behave like hammer+left/hammer+right.
-eventTaps:createEventTap({
-    hs.eventtap.event.types.otherMouseDown
-}, function (event)
-    if hammer.isDown then
-        local button = event:getProperty(hs.eventtap.event.properties.mouseEventButtonNumber)
-        local isRepeat = event:getProperty(hs.eventtap.event.properties.keyboardEventAutorepeat)
-        if isRepeat > 0 then
-            logger.i("Ignoring otherMouseDown auto repeat")
-            return true -- ignore and discard
+function onOtherMouseDownWithHammer(event)
+    local button = event:getProperty(hs.eventtap.event.properties.mouseEventButtonNumber)
+    local flags = event:getFlags()
+    if button == 3 then
+        if flags.shift then
+            windowManagement.moveLeftBig()
+        elseif flags.alt then
+            windowManagement.moveLeftSmall()
+        elseif flags.cmd then
+            windowManagement.moveWest()
+        else
+            windowManagement.moveLeft()
         end
-        local flags = event:getFlags()
-        if button == 3 then
-            if flags.shift then
-                windowManagement.moveLeftBig()
-            elseif flags.alt then
-                windowManagement.moveLeftSmall()
-            elseif flags.cmd then
-                windowManagement.moveWest()
-            else
-                windowManagement.moveLeft()
-            end
-        elseif button == 4 then
-            if flags.shift then
-                windowManagement.moveRightBig()
-            elseif flags.alt then
-                windowManagement.moveRightSmall()
-            elseif flags.cmd then
-                windowManagement.moveEast()
-            else
-                windowManagement.moveRight()
-            end
+    elseif button == 4 then
+        if flags.shift then
+            windowManagement.moveRightBig()
+        elseif flags.alt then
+            windowManagement.moveRightSmall()
+        elseif flags.cmd then
+            windowManagement.moveEast()
+        else
+            windowManagement.moveRight()
         end
-        return true -- discard
     end
-end)
-
-local function onMouseBackForward(appName, onBack, onForward)
-    eventTaps:createEventTap({
-        hs.eventtap.event.types.otherMouseDown
-    }, function (event)
-        if hammer.isDown then
-            -- a little hacky, but ignore this handler if the hammer key is down, that's handled in the code above
-            return false
-        end
-        local app = hs.application.frontmostApplication()
-        -- logger.i('otherMouseDown event, app: ' .. app:name())
-        if app:name() == appName then
-            local button = event:getProperty(hs.eventtap.event.properties.mouseEventButtonNumber)
-            local isRepeat = event:getProperty(hs.eventtap.event.properties.keyboardEventAutorepeat)
-            logger.i('otherMouseDown event, app: ' .. app:name() .. ', button: ' .. button .. ", isRepeat: " .. isRepeat)
-            if isRepeat > 0 then
-                logger.i(appName .. ": Discarding repeat button 3/4")
-                return true -- ignore and discard
-            end
-            -- implement my own debounce since I'm getting rapid/duplicate events, and they're not considered autorepeat
-            local timeSinceLastDown = util.getCurrentMilliseconds() - otherMouseDownAt
-            otherMouseDownAt = util.getCurrentMilliseconds()
-            if timeSinceLastDown < OTHER_MOUSE_DOWN_DEBOUNCE_TIME then
-                print('Ignoring fast repeat press of otherMouse button (' .. timeSinceLastDown .. 'ms)')
-                return true -- ignore and discard
-            end
-            if button == 3 then
-                logger.i(appName .. ": intercepted button3")
-                onBack(event, app)
-            elseif button == 4 then
-                logger.i(appName .. ": intercepted button4")
-                onForward(event, app)
-            end
-            return true -- discard
-        end
-    end)
+    return true -- discard
 end
 
--- onMouseBackForward(
+function onOtherMouseDownCheckCustomHandlers(event)
+    local app = hs.application.frontmostApplication()
+    local appName = app:name()
+    logger.i('onOtherMouseDownCheckCustomHandlers event, app: ' .. appName)
+    local handler = otherMouseDownCustomHandlers[appName]
+    if not handler then
+        logger.i("No custom handler for " .. appName .. ", passing on")
+        return false -- propogate
+    end
+    local button = event:getProperty(hs.eventtap.event.properties.mouseEventButtonNumber)
+    logger.i('onOtherMouseDownCheckCustomHandlers event, app: ' .. appName .. ', button: ' .. button)
+    if button == 3 then
+        logger.i(appName .. ": intercepted button3")
+        handler.onBack(event, app)
+    elseif button == 4 then
+        logger.i(appName .. ": intercepted button4")
+        handler.onForward(event, app)
+    end
+    return true -- discard
+end
+
+eventTaps:createEventTap({
+    hs.eventtap.event.types.otherMouseDown
+}, function(event)
+    local isRepeat = event:getProperty(hs.eventtap.event.properties.keyboardEventAutorepeat)
+    if isRepeat > 0 then
+        logger.i("Ignoring otherMouseDown auto repeat")
+        return true -- ignore and discard
+    end
+
+    -- implement debouncer since I'm getting unwanted double clicks, and they're not autorepeats
+    local timeSinceLastDown = util.getCurrentMilliseconds() - otherMouseDownAt
+    otherMouseDownAt = util.getCurrentMilliseconds()
+    if timeSinceLastDown < OTHER_MOUSE_DOWN_DEBOUNCE_TIME then
+        print('IGNORING UNWANTED DOUBLE CLICK of otherMouse button (' .. timeSinceLastDown .. 'ms)')
+        return true -- ignore and discard
+    end
+
+    if hammer.isDown then
+        return onOtherMouseDownWithHammer(event)
+    end
+
+    return onOtherMouseDownCheckCustomHandlers(event)
+end)
+
+local function registerCustomMouseBackForwardHandler(appName, onBack, onForward)
+    otherMouseDownCustomHandlers[appName] = {
+        onBack = onBack,
+        onForward = onForward
+    }
+end
+
+-- registerCustomMouseBackForwardHandler(
 --     "Google Chrome", 
 --     function(event, app)
 --         app:selectMenuItem({"History", "Back"})
@@ -94,7 +101,7 @@ end
 --     end
 -- )
 
--- onMouseBackForward(
+-- registerCustomMouseBackForwardHandler(
 --     "Brave Browser", 
 --     function(event, app)
 --         app:selectMenuItem({"History", "Back"})
@@ -104,7 +111,7 @@ end
 --     end
 -- )
 
-onMouseBackForward(
+registerCustomMouseBackForwardHandler(
     "Visual Studio",
     function()
         app:selectMenuItem({"Search", "Navigation History", "Navigate Back"})
@@ -114,7 +121,7 @@ onMouseBackForward(
     end
 )
 
-onMouseBackForward(
+registerCustomMouseBackForwardHandler(
     "Notes",
     function(event, app)
         -- hs.eventtap.keyStroke({'cmd', 'alt'}, '[')

5cd34539c4ea4ef3a51f50469f37508b76b3de39
Steve Kehlet <steve.kehlet@avaneerhealth.com>
2024-10-30 10:45:29 -0700

    workaround another Outlook web mail annoyance

--- include/msAppTweaks.lua
+++ include/msAppTweaks.lua
@@ -1,4 +1,6 @@
 local eventTaps = require("lib.eventTaps")
+-- local util = require("lib.util")
+local logger = hs.logger.new('caffeine.lua', 'debug')
 
 -- If using O365 Outlook or Calendar, intercept option-delete and
 -- instead send: option-shift-left then delete
@@ -17,6 +19,26 @@ local eventTaps = require("lib.eventTaps")
 --         end
 --     end
 -- end)
+
+-- If using O365 Outlook webmail client, discard alt up/down notifications. This
+-- is to workaround an annoying (to me) behavior where Outlook will intercept an
+-- option down+up sequence, when it's inadvertently done without modifying
+-- another key, and show Windows-style alt hotkeys in the Outlook web app,
+-- preventing subsequent use of the option key for moving backwards/forwards
+-- between words (option-left, option-right).
+-- Logic:
+-- If Outlook web mail is the current focused application
+-- AND the flagsChanged event is for the alt key (don't care either up or down)
+-- THEN, discard the event
+eventTaps:createEventTap({
+    hs.eventtap.event.types.flagsChanged
+}, function(event)
+    local win = hs.window.focusedWindow()
+    if win and win:title():find('- Outlook -') and event:getKeyCode() == hs.keycodes.map['alt'] then
+        logger.i("O365 Mail alt key workaround, ignoring alt flags changed event")
+        return true -- Discard the raw alt key up or down
+    end
+end)
 
 -- If using Excel, try to fix some very un-mac-like keybindings
 eventTaps:createEventTap({
